66b18995ac4b4c663a964d53767ff9255e060a5d
Sarthak Upadhyay <u.sarthak2005@gmail.com>
2025-10-01 19:24:59 +0530

    Changed layout added Progress and Completed column

--- src/ToDoLIst.jsx
+++ src/ToDoLIst.jsx
@@ -1,10 +1,13 @@
 import React, {useState} from "react";
+
 function ToDoList(){
     const [tasks,setTasks] = useState([]);
     const [newTask,setNewTask] = useState("");
+    
     function inputChange(e){
-       setNewTask(e.target.value);
+        setNewTask(e.target.value);
     }
+    
     function addTask(){
         if(newTask.trim() === "") {
             return;
@@ -12,28 +15,43 @@ function ToDoList(){
         setTasks(t => [...t, newTask]); 
         setNewTask("");
     }
-    function deleteTask(index){
-        setTasks(tasks.filter((e,i) => i !== index));
-
     
+    function deleteTask(index){
+        setTasks(t => t.filter((_, i) => i !== index));
     }
-    return(<div className="to-do-list">
-     <h1>To-Do-List</h1>
-     <div>
-        <input type="text"
-        placeholder="Enter task....."
-        value={newTask}
-        onChange={inputChange} 
-        />
-        <button className="add-button" onClick={addTask}>Add ToDo</button>
-
-     </div>
-     <ol>
-        {tasks.map((task,index)=>
-        <li key={index}><span className="text">{task}</span>
-        <button className="delete-button"
-        onClick={()=>deleteTask(index)}>Delete</button></li>)}
-     </ol>
-    </div>)
+    
+    return(
+        
+        <div className="layout-container"> 
+            
+            <div className="input-row">
+                <input 
+                    type="text"
+                    placeholder="Enter task....."
+                    value={newTask}
+                    onChange={inputChange} 
+                />
+                <button className="add-button" onClick={addTask}>Add ToDo</button>
+            </div>
+            <div className="task-column">
+                <h3>To-Do</h3>
+                <ol>
+                    {tasks.map((task,index)=>
+                    <li key={index}><span className="text">{task}</span>
+                    <button className="delete-button"
+                    onClick={()=>deleteTask(index)}>Delete</button></li>)}
+                </ol>
+            </div>
+            
+            <div className="progress-column">
+                <h3>Doing</h3>
+            </div>
+            
+            <div className="completecolumn">
+                <h3>Completed Tasks</h3>
+            </div>
+            
+        </div>
+    );
 }
 export default ToDoList;
