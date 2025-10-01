e8029fe6f7302a7de4d47d8c291736254706ec7e
Sarthak Upadhyay <u.sarthak2005@gmail.com>
2025-10-01 16:25:09 +0530

    Creating a basic structure

---
+++ src/ToDoLIst.jsx
@@ -0,0 +1,34 @@
+import React, {useState} from "react";
+function ToDoList(){
+    const [tasks,setTasks] = useState(["eat","sleep"]);
+    const [newTask,setNewTask] = useState("");
+    function inputChange(e){
+       setNewTask(e.target.value);
+    }
+    function addTask(){
+
+    }
+    function deleteTask(index){
+
+    }
+    
+    return(<div className="to-do-list">
+     <h1>To-Do-List</h1>
+     <div>
+        <input type="text"
+        placeholder="Enter task....."
+        value={newTask}
+        onChange={inputChange} 
+        />
+        <button className="add-button" onClick={addTask}>Add ToDo</button>
+
+     </div>
+     <ol>
+        {tasks.map((task,index)=>
+        <li key={index}><span className="text">{task}</span>
+        <button className="delete-button"
+        onClick={()=>deleteTask(index)}>Delete</button></li>)}
+     </ol>
+    </div>)
+}
+export default ToDoList;
